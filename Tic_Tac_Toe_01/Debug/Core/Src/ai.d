c92c1d5cd8a99db514598acc1da310dfa63248c0
mekkev <kevin.mekelburg@atos.net>
2026-03-28 00:04:28 +0100

    gitignore

--- Tic_Tac_Toe_01/Debug/Core/Src/ai.d
+++
@@ -1,4 +0,0 @@
-Core/Src/ai.o: ../Core/Src/ai.c ../Core/Src/../Inc/ai.h \
- ../Core/Src/../Inc/game.h
-../Core/Src/../Inc/ai.h:
-../Core/Src/../Inc/game.h:
--- Tic_Tac_Toe_01/Debug/Core/Src/game.d
+++
@@ -1,2 +0,0 @@
-Core/Src/game.o: ../Core/Src/game.c ../Core/Src/../Inc/game.h
-../Core/Src/../Inc/game.h:
